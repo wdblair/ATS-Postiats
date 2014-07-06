13d5a8bc8bb424a2961378d37ef058463e22903c
Will Blair <wdblair@bu.edu>
2014-07-06 13:05:59 -0400

    made the qsort example better

--- doc/EXAMPLE/PCPV/EXTERN/main.c
+++ doc/EXAMPLE/PCPV/EXTERN/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <assert.h>
 
 /**
    Here we demonstrate our code being used as a normal libc
    function in your typical C code.
- */
+*/
 extern
 void libc_qsort (void *base, size_t nmemb, size_t size,
                  int (*cmp)(const void *, const void *));
@@ -17,16 +18,22 @@ int cmp_int (const void *x, const void *y) {
   return ix - iy;
 }
 
-int main () {
-  
-  int buf[10] = {10, 9 , 8 , 7, 6, 5 , 4, 3, 2, 1};
-  
-  libc_qsort (buf, 10, sizeof(int), cmp_int);
-  
-  for (int i = 0; i < 9; i++) {
-    assert(buf[i] < buf[i+1]);
-    assert(buf[i] == buf[i+1] - 1);
-  }
+#define N 1048576
 
+int main () {
+  int *buf = calloc(N, sizeof(int));
+  
+  assert(buf != NULL);
+
+  for (int i = 0; i < N; i++) {
+    buf[i] = (int)random() % INT_MAX;
+  }
+  
+  libc_qsort (buf, N, sizeof(int), cmp_int);
+  
+  for (int i = 0; i < (N - 1); i++) {
+    assert(buf[i] <= buf[i+1]);
+  }
+  
   printf("Sorting test passed!\n");
 }
